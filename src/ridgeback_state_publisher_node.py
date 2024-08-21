473705a722cdd2a1801980c4147e93c3ce593108
TracyDuX <xintong.du@robotics.utias.utoronto.ca>
2024-08-21 15:03:33 -0400

    update tf tree in vicon

--- src/ridgeback_state_publisher_node.py
+++ src/ridgeback_state_publisher_node.py
@@ -6,7 +6,7 @@ import sys
 import tf
 import tf2_ros
 from geometry_msgs.msg import TransformStamped
-from tf.transformations import quaternion_from_euler
+from tf.transformations import quaternion_from_euler, rotation_matrix,inverse_matrix, quaternion_from_matrix
 from sensor_msgs.msg import JointState
 
 class RidgebackStatePublisherNode:
@@ -31,12 +31,13 @@ class RidgebackStatePublisherNode:
 
         self.baselink_map_tf = TransformStamped()
         self.baselink_map_tf.header.stamp = rospy.Time.now()
-        self.baselink_map_tf.header.frame_id = "world"
-        self.baselink_map_tf.child_frame_id = "base_link"
+        self.baselink_map_tf.header.frame_id = "base_link"
+        self.baselink_map_tf.child_frame_id = "world"
 
         self.sub = rospy.Subscriber(args.joint_states_topic, JointState, self._joint_states_cb)
 
     def _joint_states_cb(self, msg):
+        '''
         if not self.odom_init:
             self.odom_map_tf.transform.translation.x = msg.position[0]
             self.odom_map_tf.transform.translation.y = msg.position[1]
@@ -53,13 +54,18 @@ class RidgebackStatePublisherNode:
         # self.odom_map_tf.header.stamp = rospy.Time.now()
         self.odom_map_tf.header.stamp = msg.header.stamp
         self.broadcaster_static.sendTransform(self.odom_map_tf)
-
+        '''
         # self.baselink_map_tf.header.stamp = rospy.Time.now()
+
+        Twb = rotation_matrix(msg.position[2], (0,0,1))
+        Twb[0, 3] = msg.position[0]
+        Twb[1, 3] = msg.position[1]
+        Tbw = inverse_matrix(Twb)
+        quat = quaternion_from_matrix(Tbw)
         self.baselink_map_tf.header.stamp = msg.header.stamp
-        self.baselink_map_tf.transform.translation.x = msg.position[0]
-        self.baselink_map_tf.transform.translation.y = msg.position[1]
+        self.baselink_map_tf.transform.translation.x = Tbw[0, 3]
+        self.baselink_map_tf.transform.translation.y = Tbw[1, 3]
         self.baselink_map_tf.transform.translation.z = 0.
-        quat = quaternion_from_euler(0., 0., msg.position[2])
         self.baselink_map_tf.transform.rotation.x = quat[0]
         self.baselink_map_tf.transform.rotation.y = quat[1]
         self.baselink_map_tf.transform.rotation.z = quat[2]
