caecd6732a9218e6ffbf55a330ec109939e80ef8
Jingxing Qian <jingxing.qian@mail.utoronto.ca>
2024-09-16 16:17:17 -0400

    modify plotting script

--- scripts/analysis/plot_ridgeback_state_estimates.py
+++ scripts/analysis/plot_ridgeback_state_estimates.py
@@ -21,19 +21,18 @@ def main():
     tb_ests, qb_ests, vb_ests = ros_utils.parse_ridgeback_joint_state_msgs(est_msgs)
     vb_nums = np.diff(qbs, axis=0).T/np.diff(tbs)
     vb_nums = vb_nums.T
-    if (len(tbs) > 0):
-        tbs = tbs - tbs[0]
-    tb_ests = tb_ests - tb_ests[0]
+
+    tbs = tbs - tbs[0]
+    tb_ests = tb_ests - tb_ests[0] + tbs[0]
 
     # TODO trim messages to only start once we get a command
     plt.figure()
-    if (len(tbs) > 0):
-        plt.plot(tbs, qbs[:, 0], label="x_vicon")
-        plt.plot(tbs, qbs[:, 1], label="y_vicon")
-        plt.plot(tbs, qbs[:, 2], label="θ_vicon")
-    plt.plot(tb_ests, qb_ests[:, 0], label="x_est")
-    plt.plot(tb_ests, qb_ests[:, 1], label="y_est")
-    plt.plot(tb_ests, qb_ests[:, 2], label="θ_est")
+    plt.plot(tbs, qbs[:, 0]-qbs[0, 0], label="x_vicon")
+    plt.plot(tbs, qbs[:, 1]-qbs[0, 1], label="y_vicon")
+    plt.plot(tbs, qbs[:, 2]-qbs[0, 2], label="θ_vicon")
+    plt.plot(tb_ests, qb_ests[:, 0]-qb_ests[0, 0], label="x_est")
+    plt.plot(tb_ests, qb_ests[:, 1]-qb_ests[0, 1], label="y_est")
+    plt.plot(tb_ests, qb_ests[:, 2]-qb_ests[0, 2], label="θ_est")
     plt.title("Ridgeback Joint Positions")
     plt.xlabel("Time (s)")
     plt.ylabel("Joint position")
@@ -41,10 +40,9 @@ def main():
     plt.grid()
 
     plt.figure()
-    if (len(tbs) > 0):
-        plt.plot(tbs[:-1], vb_nums[:, 0], label="vx_vicon")
-        plt.plot(tbs[:-1], vb_nums[:, 1], label="vy_vicon")
-        plt.plot(tbs[:-1], vb_nums[:, 2], label="vθ_vicon")
+    plt.plot(tbs[:-1], vb_nums[:, 0], label="vx_vicon")
+    plt.plot(tbs[:-1], vb_nums[:, 1], label="vy_vicon")
+    plt.plot(tbs[:-1], vb_nums[:, 2], label="vθ_vicon")
     plt.plot(tb_ests, vb_ests[:, 0], label="vx_est")
     plt.plot(tb_ests, vb_ests[:, 1], label="vy_est")
     plt.plot(tb_ests, vb_ests[:, 2], label="ω_est")
