72010ec2936103c926f5c3a23e7dfc8117ae2539
Jingxing Qian <jingxing.qian@mail.utoronto.ca>
2025-05-21 13:18:20 -0400

    add more record topics

--- scripts/logging/record_bag.sh
+++ scripts/logging/record_bag.sh
@@ -8,6 +8,18 @@ mkdir -p "$BAG_DIR/$1"
    --regex "/ridgeback_velocity_controller/(.*)" \
    --regex "/ur10/(.*)" \
    --regex "/vicon/(.*)" \
-   --regex "/mpc_tracking_pt"
+   --regex "/mpc_tracking_pt" \
+   --regex "/semantic_pointcloud" \
+   --regex "/tf" \
+   --regex "/tf_static" \
+   --regex "/controller_visualization" \
+   --regex "/controller_visualization_array" \
+   --regex "/plan_visualization" \
+   --regex "/pose_plan_visualization" \
+   --regex "/current_plan_visualization" \
+   --regex "/controller_reference" \
+   --regex "/controller_tracking_pt" \
+   --regex "/pocd_slam_node/occupied_ef_nodes" \
+   --regex "/revis_node/tracked_objects_cloud"
 
 #rosbag record -a -o "$BAG_DIR/$1"
